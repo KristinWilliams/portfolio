33327c46003c7dea085c6db86c90bd8e16b9a6c0
KristinWilliams <wkristin71@yahoo.co.uk>
2022-05-26 20:01:04 -0500

    finished base jsx

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,25 @@ import React from "react";
 
 const App = () => {
   return (
-    <div class="container">
-      <div class="hero-container">
+    <div className="container">
+      <div className="hero-container">
         <nav>
           <img src="img/laptop-icon.svg" alt="logo icon" />
-          <ul class="nav-list">
+          <ul className="nav-list">
             <li>skills</li>
             <li>work</li>
             <li>contract</li>
           </ul>
         </nav>
-        <div class="name-container">
+        <div className="name-container">
           <h1>kristin williams</h1>
           <h2>Frontend Developer</h2>
         </div>
         <img src="img/down-arrow.svg" alt="down arrow" />
       </div>
-      <div class="skills-container">
+      <div className="skills-container">
         <h1>skills</h1>
-        <ul class="skills-list">
+        <ul className="skills-list">
           <li>HTML</li>
           <li>SCSS</li>
           <li>RESTApi</li>
@@ -29,9 +29,27 @@ const App = () => {
           <li>React</li>
         </ul>
       </div>
-      <div class="work-container">
+      <div className="work-container">
         <h1>work</h1>
-        <div class="project-container"></div>
+        <div className="project">
+          <div className="project-container">
+            <img src="img/github-icon.svg" />
+            <img src="img/link-icon.svg" />
+            <h1>All About it</h1>
+            <h3 className="language-used">React</h3>
+          </div>
+        </div>
+      </div>
+      <div className="contact-container">
+        <h1>contact</h1>
+        <img src="img/github-icon.svg" />
+        <img src="img/link-icon.svg" />
+        <form name="contact" method="POST">
+          <input type="text" placeholder="Name"></input>
+          <input type="text" placeholder="Email"></input>
+          <input type="textarea" placeholder="Comment"></input>
+          <button type="submit">submit</button>
+        </form>
       </div>
     </div>
   );
